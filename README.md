b67f43f903ca732cc1707925dd6d75730773ad6d
asadulislam <asadulislam0288@gmail.com>
2025-09-10 22:41:02 +0600

    added a6-green-earth-buttonClick-RedmiFile

---
+++ README.md
@@ -0,0 +1,34 @@
+#### 1) What is the difference between var, let, and const?
+
+var পুরোনো পদ্ধতি, function-scoped, flexible কিন্তু সমস্যা তৈরি করতে পারে।
+
+let আধুনিক block-scoped update করা যায়।
+
+const আধুনিক block-scoped একবার ভ্যালু সেট করলে আর পরিবর্তন করা যায় না।
+
+#### 2) What is the difference between map(), forEach(), and filter()?
+
+Ans: forEach() শুধু কাজ করবে, কিছু return করবে না।
+
+map() কাজ করে নতুন array বানাবে।
+
+filter() শর্ত মেনে নতুন array বানাবে।
+
+#### 3) What are arrow functions in ES6?
+
+Ans: দেখছো অনেক ছোট আর সহজ।
+ফাংশন কিওয়ার্ড দরকার হয় না।
+
+রিটার্ন এক লাইনে লিখলে রিটার্ন লিখতে হয় না।
+
+#### 4) How does destructuring assignment work in ES6?
+
+Ans; Array Destructuring : index এর ভিত্তিতে value বের করে।
+
+Object Destructuring : key এর নাম অনুযায়ী value বের করে।
+
+#### 5) Explain template literals in ES6. How are they different from string concatenation?
+
+Concatenation = + ব্যবহার করে string জোড়া লাগে, কোড লম্বা হয়।
+
+Template Literal = backtick + ${} ব্যবহার করে সহজে লেখা যায়, multi-line ও করা যায়।
--- script/script.js
+++ script/script.js
@@ -4,7 +4,16 @@ const lodedCategories = () => {
     .then((res) => res.json())
     .then((data) => displayCategories(data.categories));
 };
-
+// loading
+const manageloding = (status) => {
+  if (status == true) {
+    document.getElementById("spainar").classList.remove("hidden");
+    document.getElementById("main-cointener").classList.add("hidden");
+  } else {
+    document.getElementById("main-cointener").classList.remove("hidden");
+    document.getElementById("spainar").classList.add("hidden");
+  }
+};
 // button bg remove
 const removeActive = () => {
   const treeButton = document.querySelectorAll(".tree-btn");
@@ -13,6 +22,7 @@ const removeActive = () => {
 };
 
 const loadCointener = (id) => {
+  manageloding(true);
   const url = `https://openapi.programming-hero.com/api/category/${id}`;
   fetch(url)
     .then((res) => res.json())
@@ -38,7 +48,7 @@ const displayTreeDtails = (catagory) => {
   console.log(catagory);
   const detailsTrees = document.getElementById("details-cointener");
   detailsTrees.innerHTML = `<div>
-             <h1 class="font-bold text-xl">${catagory.name}</h1>
+             <h1 class="font-bold text-xl font-inter">${catagory.name}</h1>
            <div class="w-full h-40 sm:h-48 md:h-56 lg:h-60 mb-4 overflow-hidden rounded">
       <img
         class="w-full h-full object-cover"
@@ -47,14 +57,14 @@ const displayTreeDtails = (catagory) => {
       />
     </div>
             <p class="text-xl font-semibold">
-              Category: <span class="text-gray-500 text-[20px]">${catagory.category}</span>
+              Category: <span class="text-gray-500 text-[20px] font-inter">${catagory.category}</span>
             </p>
             <p class="text-xl font-semibold">
-              Price: <span class="text-gray-500 text-[20px]">${catagory.price}</span>
+              Price: <span class="text-gray-500 text-[20px] font-inter"><i class="fa-solid fa-bangladeshi-taka-sign fa-xs"></i>${catagory.price}</span>
             </p>
             <p class="text-xl font-semibold">
               Description:
-              <span class="text-gray-500 text-[20px]"
+              <span class="text-gray-500 font-inter text-[20px]"
                 >${catagory.description}</span
               >
             </p>
@@ -96,7 +106,7 @@ const displayLoadCointener = (catagorys) => {
       >
         ${catagory.category}
       </span>
-      <span class="price font-semibold">${catagory.price}</span>
+      <span class="price font-semibold"><i class="fa-solid fa-bangladeshi-taka-sign fa-xs"></i></i>${catagory.price}</span>
     </div>
 
     <!-- Add to cart -->
@@ -109,10 +119,12 @@ const displayLoadCointener = (catagorys) => {
     `;
     catagoryCointener.append(card);
   });
+  manageloding(false);
 };
 // 2
 
 const loadCointener2 = () => {
+  manageloding(true);
   const url = `https://openapi.programming-hero.com/api/plants`;
   fetch(url)
     .then((res) => res.json())
@@ -153,7 +165,7 @@ const displayLoadCointener2 = (catagorys) => {
       >
         ${catagory.category}
       </span>
-      <span class="price font-semibold">${catagory.price}</span>
+      <span class="price font-semibold"><i class="fa-solid fa-bangladeshi-taka-sign fa-xs"></i> ${catagory.price}</span>
     </div>
 
     <!-- Add to cart -->
@@ -186,31 +198,67 @@ const displayCategories = (names) => {
     `;
     levelCategories.append(asideCategories);
   }
+  manageloding(false);
 };
 
 lodedCategories();
 loadCointener2(1);
 
-// click button
+// button click ......................................
+let cart = {};
+let totalPrice = 0;
+
 const cartList = document.getElementById("cart-list");
 const totalPriceEl = document.getElementById("total-price");
-
 document
   .getElementById("green-cointener")
   .addEventListener("click", function (e) {
     if (e.target.classList.contains("call-btn")) {
       const card = e.target.closest("div");
       const name = card.querySelector("h3").innerText;
-
       const price = parseInt(
-        card.querySelector(".font-semibold").innerText.replace("৳", "")
+        card.querySelector(".price").innerText.replace("৳", "")
       );
-      const li = document.createElement("li");
-      li.classList.add("flex", "justify-between");
-      li.innerHTML = `
-      <span>${name}</span>
-      <span>৳${price} × 1</span>
-    `;
-      cartList.appendChild(li);
+      if (cart[name]) {
+        cart[name].qty += 1;
+      } else {
+        cart[name] = { price: price, qty: 1 };
+      }
+
+      updateCart();
     }
   });
+
+cartList.addEventListener("click", function (e) {
+  if (e.target.classList.contains("remove-btn")) {
+    const name = e.target.dataset.name;
+    if (cart[name]) {
+      totalPrice -= cart[name].price * cart[name].qty;
+      delete cart[name];
+      updateCart();
+    }
+  }
+});
+
+// + Total
+function updateCart() {
+  cartList.innerHTML = "";
+  totalPrice = 0;
+
+  for (let name in cart) {
+    const item = cart[name];
+    totalPrice += item.price * item.qty;
+
+    const li = document.createElement("li");
+    li.classList.add("flex", "justify-between", "items-center");
+    li.innerHTML = `
+     <div> <span>${name}</span> <br>
+      <span>৳${item.price} × ${item.qty}</span></div>
+      <button class="remove-btn text-red-600 ml-2" data-name="${name}">❌</button>
+    `;
+    cartList.appendChild(li);
+  }
+
+  totalPriceEl.innerText = totalPrice;
+}
+// ,,,,,,,,,,,,,,,,,,,,,
